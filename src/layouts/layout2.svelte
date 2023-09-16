151698182a048e46a0b379810634ade0f2600e46
sam-eah <s.sam25@laposte.net>
2023-09-16 21:13:39 +0200

    fix swiping direction

--- src/layouts/layout2.svelte
+++ src/layouts/layout2.svelte
@@ -9,15 +9,20 @@
 
     if (location.pathname === '/cv' && event.detail.direction === 'right') {
       a.click();
+      return;
     }
 
     if (location.pathname !== '/cv' && event.detail.direction === 'left') {
       b.click();
-    }
+      return;
+    }fix s
   }
 </script>
 
-<div use:swipe={{ timeframe: 300, minSwipeDistance: 60 }} on:swipe={handler}>
+<div
+  use:swipe={{ timeframe: 300, minSwipeDistance: 60, touchAction: 'pan-y' }}
+  on:swipe={handler}
+>
   <a href="/" class="hidden" bind:this={a} />
   <a href="/cv" class="hidden" bind:this={b} />
   <slot />
